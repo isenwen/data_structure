d3a862bb57cf6e1ce0436a6ab0bd4918652dfd7a
iwen <742729764@qq.com>
2020-11-17 18:14:46 +0800

    Java 数据结构练习

---
+++ MyArray/src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private int age;
+    private String name;
+    public Person(int age, String name) {
+        super();
+        this.age = age;
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Person [age=" + age + ", name=" + name + "]";
+    }
+}
